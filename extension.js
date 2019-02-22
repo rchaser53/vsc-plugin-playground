549fe84730b33d8bc2e3741a667f1ca0f3d4da95
rchaser53 <tayoshizawa29@gmail.com>
2019-02-22 23:44:31 +0900

    use prettier

--- extension.js
+++ extension.js
@@ -1,18 +1,21 @@
-const child_process = require('child_process');
-const vscode = require('vscode');
+const child_process = require("child_process");
+const vscode = require("vscode");
 
 function activate(context) {
-	const disposable = vscode.commands.registerCommand('extension.rustfmt', (editor) => {
-    const fileName = vscode.window.activeTextEditor.document.fileName
-    child_process.spawnSync('rustfmt', ['--', fileName])
-	});
+  const disposable = vscode.commands.registerCommand(
+    "extension.rustfmt",
+    () => {
+      const fileName = vscode.window.activeTextEditor.document.fileName;
+      child_process.spawnSync("rustfmt", ["--", fileName]);
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate
+};
